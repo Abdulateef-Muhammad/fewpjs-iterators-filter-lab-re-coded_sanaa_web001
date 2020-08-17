954807adc16eee0010a1bbb0743607b16990b9b4
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-17 15:50:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+let findMatching = function(drivers, string) {
+  return drivers.filter( current => current.toLowerCase() === string.toLowerCase());
+}
+
+let fuzzyMatch = function(drivers, string) {
+  return drivers.filter( function(current) {
+    console.log(current[0])
+  });
+}
+
+let matchName = function(drivers, string) {
+  return drivers.filter( current => current.name === string);
+}
+
+let array = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
